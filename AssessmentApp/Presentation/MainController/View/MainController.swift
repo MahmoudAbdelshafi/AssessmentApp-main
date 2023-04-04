47b7bedbcec6bee1b1f9c80352c04e9b025915c6
Mahmoud <mahmoud.abdelshafi4@gmail.com>
2023-04-04 06:38:14 +0200

    refactor MainController

--- AssessmentApp/Presentation/MainController/View/MainController.swift
+++ AssessmentApp/Presentation/MainController/View/MainController.swift
@@ -8,24 +8,7 @@
 import UIKit
 import Combine
 
-class TableViewAdjustedHeight: UITableView {
-    override var intrinsicContentSize: CGSize {
-        self.layoutIfNeeded()
-        return self.contentSize
-    }
-override var contentSize: CGSize {
-        didSet {
-            self.invalidateIntrinsicContentSize()
-        }
-    }
-override func reloadData() {
-        super.reloadData()
-        self.invalidateIntrinsicContentSize()
-    }
-}
-
 class MainController: UIViewController {
-    
     
     //MARK: - Properties -
     
